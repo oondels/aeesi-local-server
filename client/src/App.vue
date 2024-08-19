e1e4fc495f62cd3781dd2ac91591b977e55e6767
Hendrius Félix <hendriusfelix@gmail.com>
2024-08-19 19:44:56 -0300

    Teste de backend

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,39 @@
 <template>
-  <h1>Aeesi</h1>
+  <div>
+    <h1>Aeesi</h1>
+    <button @click="TesteNames">Click me</button>
+    <div class="names" v-for="(name, nameIndex) in names" :key="nameIndex">
+      {{ name }}
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+import ip from "../../ip";
+
 export default {
   name: "App",
   components: {},
+
+  data() {
+    return {
+      names: null,
+    };
+  },
+
+  methods: {
+    TesteNames() {
+      axios
+        .get(`${ip}/user`)
+        .then((response) => {
+          this.names = response.data.names;
+        })
+        .catch((error) => {
+          console.error("Erro ao se comunicar com servidor: ", error);
+        });
+    },
+  },
 };
 </script>
 
